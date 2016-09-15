e5aae872f9d65d1aafd2c27e910e16c8766f7be9
Jeffrey Silverman <jeffreysilverman@gmail.com>
2016-09-15 13:01:49 -0700

    added print art func

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -1,2 +1,17 @@
 def print_message():
     print 'hi'
+    
+def print_art():
+    print ''
+    print "            __,__"
+    print '   .--.  .-"     "-.  .--.'
+    print "  / .. \/  .-. .-.  \/ .. \ "
+    print " | |  '|  /   Y   \  |'  | |"
+    print " | \   \  \ 0 | 0 /  /   / |"
+    print "  \ '- ,\.-'`` ``'-./, -' /"
+    print "   `'-' /_   ^ ^   _\ '-'`"
+    print "       |  \._   _./  |"
+    print "       \   \ `~` /   /"
+    print "        '._ '-=-' _.'"
+    print "           '~---~'"
+
